372bde54100b57736ca6f076027c8c9ebc0ad32b
yaung3 <78059165+yaung3@users.noreply.github.com>
2021-03-14 23:21:01 -0700

    doc-strings added

--- web_tictactoe.py
+++ web_tictactoe.py
@@ -20,10 +20,14 @@ async def get_game(game_id):
 
 async def create_user(client, message_prop):
     """
-    
-    :param client: the MQTT brooker client
-    :param message_prop:
-    :return:
+    Creates a user in the data base.
+
+    Topic: commands
+    MQTT Message: create_user username
+
+    :param client: the MQTT broker client
+    :param message_prop: username desired
+    :return:username and password
     """
     async with Client("test.mosquitto.org") as client:
         await client.subscribe("commands")
@@ -40,6 +44,14 @@ async def create_user(client, message_prop):
 
 
 async def create_game(client):
+    """
+    Creates a Tic Tac Toe session
+
+    Topic: commands
+    MQTT Message: create_game
+    :param client: MQTT broker client
+    :return: the game_id and termination password of the created game
+    """
     try:
         game_uuid, term_password = await TICTACTOE_DB.add_game()
         await client.publish(("games/" + str(game_uuid)) + "/success", True, qos =2)
@@ -50,6 +62,15 @@ async def create_game(client):
 
 
 async def add_player_to_game(client, message_prop):
+    """
+    Adds a player to the existing Tic Tac Toe game.
+
+    Topic: commands
+    MQTT message: add_player_to_game game_uuid, username
+
+    :param client: MQTT client
+    :param message_prop: game_uuid = ID of the game, username: username of the user to be added
+    """
     message_prop_part = message_prop.split(",")
     game_uuid = message_prop_part[0]
     username = message_prop_part[1]
@@ -60,11 +81,20 @@ async def add_player_to_game(client, message_prop):
         await client.publish(("games/" + str(game_uuid) + "/error"), False, qos=2)
         raise MqttError("The game is full. Please choose another game.")
     else:
-        await client.publish(("games/" + str(game_uuid) + "/players" + str(username)),"added as player", qos=2 )
+        await client.publish(("games/" + str(game_uuid) + "/players" + str(username)),"added as player", qos=2)
         await asyncio.sleep(2)
 
 
 async def initialize(client, game_uuid):
+    """
+    Initialize the Tic Tac Toe game.
+
+    Topic: commands
+    MQTT message: initialize game_uuid
+
+    :param client: MQTT broker client
+    :param game_uuid: ID of the Tic Tac Toe game
+    """
     the_game, game_info = await TICTACTOE_DB.get_game(game_uuid)
     player1, player2 = game_info.players
     the_game.player_menu(player1, player2)
@@ -78,6 +108,16 @@ async def initialize(client, game_uuid):
 
 
 async def current_sign(client, game_uuid):
+    """
+    Gives the current sign (X or O) of the current player.
+
+    Topic: commands
+    MQTT message: current_sign game_uuid
+
+    :param client: MQTT Broker Client
+    :param game_uuid: the ID of the game
+    :return: the sign of current player
+    """
     the_game, game_info = await TICTACTOE_DB.get_game(game_uuid)
     curr_sign = the_game.current_sign()
     await client.publish(("games/" + str(game_uuid) + "/current_sign/"), str(curr_sign),qos=2)
@@ -85,12 +125,27 @@ async def current_sign(client, game_uuid):
 
 
 async def switch_player(game_uuid):
+    """
+    Switch turns after a player has completed the turn.
+
+    :param game_uuid: ID of the game
+    :return: the switched players
+    """
     the_game, game_info = await TICTACTOE_DB.get_game(game_uuid)
     player1, player2 = game_info.players
     the_game.switch_player(player1, player2)
 
 
 async def player_move(client, message_prop):
+    """
+    Execute a player's requested move.
+
+    Topic: commands
+    MQTT message: player_move game_uuid, move
+
+    :param client: MQTT Broker Client
+    :param message_prop: game_uuid = ID of the game, move = Grid number of where the players would like to place their sign
+    """
     message_prop_part = message_prop.split(",")
     game_uuid = message_prop_part[0]
     move = message_prop_part[1]
@@ -106,6 +161,15 @@ async def player_move(client, message_prop):
 
 
 async def winner_check(client, game_uuid):
+    """
+    Checks if there's a winner in the game or if the game is drawn
+
+    Topic: commands
+    MQTT message: winner_check game_uuid
+
+    :param client: MQTT broker client
+    :param game_uuid: the ID of the game in interest
+    """
     the_game, game_info = await TICTACTOE_DB.get_game(game_uuid)
     curr_sign = await the_game.current_sign()
     curr_player = the_game.get_current_player()
@@ -118,6 +182,11 @@ async def winner_check(client, game_uuid):
 
 
 async def message_sort():
+    """
+    Analyzes the user message and determine which functions are to be executed.
+
+    :return: Functions to be executed
+    """
     async with Client("test.mosquitto.org") as client:
         await client.subscribe("commands")
         async with client.unfiltered_messages() as messages:
@@ -138,6 +207,10 @@ async def message_sort():
                 elif message_line.startswith("initialize"):
                     game_uuid = message_line.replace("initialize", '')
                     await add_player_to_game(client, game_uuid)
+
+                elif message_line.startswith("current_sign"):
+                    game_uuid = message_line.replace("current_sign", '')
+                    await current_sign(client, game_uuid)
 
                 elif message_line.startswith("player_move"):
                     message_prop = message_line.replace("player_move", '')
